f45ed2cce05b141df353479ab7895e3451237626
40030361 <jyoung22@qub.ac.uk>
2021-03-13 19:02:25 +0000

    Updates the constructor, setters, and removes redundant code.
    Constructor signature and body updated to only require the square name. The position of the square will be set by the Board object, therefore squarePosition was removed from the constructor.
    
    The validation within the setters has been updated to validate against constants set in the Game object.
    
    The variable squareSystem and associated methods have been removed. This is because a square cannot be part of a System, only Components can do this.

--- ArtemisLite/src/artemis/lite/Square.java
+++ ArtemisLite/src/artemis/lite/Square.java
@@ -1,119 +1,94 @@
 package artemis.lite;
 
+/**
+ * Base class for virtual board square. This object can only contain a name and position.
+ *
+ * @author John Young 40030361
+ */
 public class Square {
-	
-	// instance vars
-	
-	/**
-	 * holds a value of type string for square name
-	 */
-	private String squareName;
-	
-	/**
-	 * holds a value of type string for square position
-	 */
-	private int squarePosition;
-	
-	/**
-	 * represents the system a square is allocated to
-	 */
-	private ArtemisSystem squareSystem;
-	
-	
-	
-	// constructors
-	
-	/**
-	 * default constructor
-	 */
-	public Square() {
-		
-	}
-	
-	
-	/**
-	 * constructor with args returning squareName and squarePosition
-	 * for each object
-	 * Business rules applied
-	 * @param squareName
-	 * @param squarePosition
-	 */
-	public Square(String squareName, int squarePosition, ArtemisSystem squareSystem) {
-		this.setSquareName(squareName);
-		this.setSquarePosition(squarePosition);
-		this.squareSystem = squareSystem;
-	}
 
-	
-	// methods
+    // instance vars
 
-	/**
-	 * @return the squareName
-	 */
-	public String getSquareName() {
-		return squareName;
-	}
-	
-	
-	/**
-	 * @param squareName the squareName to set
-	 */
-	public void setSquareName(String squareName) throws IllegalArgumentException {
-		
-		if (squareName.length() > 0) {
-			this.squareName = squareName;
-		} else {
-			throw new IllegalArgumentException("Invalid Square Name");
-		}
-	}
-	
-	
-	/**
-	 * @return the squarePosition
-	 */
-	public int getSquarePosition() {
-		return squarePosition;
-	}
-	
-	
-	/**
-	 * Position of square must be within the range of 1-12 inclusive
-	 * @param squarePosition the squarePosition to set
-	 */
-	public void setSquarePosition(int squarePosition) throws IllegalArgumentException {
-		
-		if ((squarePosition >= 1) && (squarePosition <= 12)) {
-			this.squarePosition = squarePosition;
-		} else {
-			throw new IllegalArgumentException("Invalid Position");
-		}
-	}
-	
-	
-	/**
-	 * @return the squareSystem
-	 */
-	public ArtemisSystem getSquareSystem() {
-		return squareSystem;
-	}
+    /**
+     * holds a value of type string for square name
+     */
+    private String squareName;
+
+    /**
+     * holds a value of type string for square position
+     */
+    private int squarePosition;
 
 
-	/**
-	 * @param squareSystem the squareSystem to set
-	 */
-	public void setSquareSystem(ArtemisSystem squareSystem) {
-		this.squareSystem = squareSystem;
-	}
-	
-	
-	/**
-	 * displays all details associated with the object
-	 */
-	public void displayAllDetails() {
-		System.out.println("Name \t:" + this.squareName);
-		System.out.println("Position \t:" + this.squarePosition);
-		
-		// this method can be inherited by component class
-	
-	}
+    // constructors
+
+    /**
+     * default constructor
+     */
+    public Square() {
+
+    }
+
+    /**
+     * Constructor for creating square object with only the name of the square
+     *
+     * @param squareName
+     */
+    public Square(String squareName) {
+        this.setSquareName(squareName);
+    }
+
+    // methods
+
+    /**
+     * @return the squareName
+     */
+    public String getSquareName() {
+        return squareName;
+    }
+
+
+    /**
+     * @param squareName the squareName to set
+     */
+    public void setSquareName(String squareName) throws IllegalArgumentException {
+        // validate name length
+        if (squareName.length() <= 0 || squareName.length() > Game.MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("Invalid name - must be between 1-" + Game.MAXIMUM_NAME_LENGTH +
+                    " characters long");
+        }
+
+        this.squareName = squareName;
+    }
+
+
+    /**
+     * @return the squarePosition
+     */
+    public int getSquarePosition() {
+        return squarePosition;
+    }
+
+
+    /**
+     * Position of square must be within the range of 1-12 inclusive
+     *
+     * @param squarePosition the squarePosition to set
+     */
+    public void setSquarePosition(int squarePosition) throws IllegalArgumentException {
+
+        if ((squarePosition >= 0) && (squarePosition <= Game.MAXIMUM_SQUARES - 1)) {
+            this.squarePosition = squarePosition;
+        } else {
+            throw new IllegalArgumentException("Invalid Position");
+        }
+    }
+
+    /**
+     * displays all details associated with the object
+     */
+    public void displayAllDetails() {
+        System.out.println("Name \t:" + this.squareName);
+        System.out.println("Position \t:" + this.squarePosition);
+    }
 }
