97a095eb3ed55b0bbcf1c0ca9404829fb312454c
40040696 <klambe03@qub.ac.uk>
2021-03-23 01:06:26 +0000

    Added ENUM to represent development stages
    Added enum to represent the various stages of a component's development
    i.e. analysing requirements, designing, building, testing, and
    maintaining.

---
+++ ArtemisLite/src/artemis/lite/DevelopmentStage.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package artemis.lite;
+
+/**
+ * @author Kieran Lambe 40040696
+ *
+ */
+public enum DevelopmentStage {
+
+	ANALYSING_REQUIREMENTS,DESIGNING,BUILDING,TESTING,MAINTAINING;
+	
+}
