5d1d77f34c83198737f4245bcc42524412a55671
40030361 <jyoung22@qub.ac.uk>
2021-03-29 15:27:13 +0100

    Adds method which will inform players how many more components they need to acquire within a system, before they can start developing them

--- ArtemisLite/src/artemis/lite/ArtemisSystem.java
+++ ArtemisLite/src/artemis/lite/ArtemisSystem.java
@@ -15,168 +15,196 @@ import java.util.List;
  */
 public class ArtemisSystem {
 
-    private String systemName;
-    private Player systemOwner;
-    private ArrayList<Component> componentsInSystem = new ArrayList<>();
+	private String systemName;
+	private Player systemOwner;
+	private ArrayList<Component> componentsInSystem = new ArrayList<>();
 
-    /**
-     * Default constructor
-     */
-    public ArtemisSystem() {
+	/**
+	 * Default constructor
+	 */
+	public ArtemisSystem() {
 
-    }
+	}
 
-    /**
-     * Overloaded constructor that only needs system name to instantiate
-     * @param systemName the name of the system
-     */
-    public ArtemisSystem(String systemName) {
-        this.setSystemName(systemName);
-    }
+	/**
+	 * Overloaded constructor that only needs system name to instantiate
+	 * @param systemName the name of the system
+	 */
+	public ArtemisSystem(String systemName) {
+		this.setSystemName(systemName);
+	}
 
-    /**
-     * @return the systemName
-     */
-    public String getSystemName() {
-        return systemName;
-    }
+	/**
+	 * @return the systemName
+	 */
+	public String getSystemName() {
+		return systemName;
+	}
 
-    /**
-     * @param systemName the systemName to set
-     */
-    public void setSystemName(String systemName) throws IllegalArgumentException {
-        // validate name length
-        if (systemName.length() <= 0 || systemName.length() > Game.MAXIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException("Invalid name - must be between 1-" + Game.MAXIMUM_NAME_LENGTH +
-                    " characters long");
-        }
+	/**
+	 * @param systemName the systemName to set
+	 */
+	public void setSystemName(String systemName) throws IllegalArgumentException {
+		// validate name length
+		if (systemName.length() <= 0 || systemName.length() > Game.MAXIMUM_NAME_LENGTH) {
+			throw new IllegalArgumentException("Invalid name - must be between 1-" + Game.MAXIMUM_NAME_LENGTH +
+					" characters long");
+		}
 
-        this.systemName = systemName;
-    }
+		this.systemName = systemName;
+	}
 
-    /**
-     * This method checks if all components in this Artemis System are developed 
-     * @return - the method returns true if all components are developed, otherwise it returns false
-     */
-    public boolean checkFullyDeveloped() {
+	/**
+	 * This method checks if all components in this Artemis System are developed
+	 * @return - the method returns true if all components are developed, otherwise it returns false
+	 */
+	public boolean checkFullyDeveloped() {
 
-        int fullyDevelopedCounter = 0;
+		int fullyDevelopedCounter = 0;
 
-        for (Component component : componentsInSystem) {
-            if (component.checkFullyDeveloped()) {
-                fullyDevelopedCounter++;
-            }
-        }
+		for (Component component : componentsInSystem) {
+			if (component.checkFullyDeveloped()) {
+				fullyDevelopedCounter++;
+			}
+		}
 
-        return fullyDevelopedCounter == componentsInSystem.size();
+		return fullyDevelopedCounter == componentsInSystem.size();
 
-    }
+	}
 
-    /**
-     * @return the systemOwner
-     */
-    public Player getSystemOwner() {
-        return systemOwner;
-    }
+	/**
+	 * @return the systemOwner
+	 */
+	public Player getSystemOwner() {
+		return systemOwner;
+	}
 
-    /**
-     * @param systemOwner the systemOwner to set
-     */
-    public void setSystemOwner(Player systemOwner) {
-        this.systemOwner = systemOwner;
-    }
+	/**
+	 * @param systemOwner the systemOwner to set
+	 */
+	public void setSystemOwner(Player systemOwner) {
+		this.systemOwner = systemOwner;
+	}
 
-    /**
-     * @return the componentsInSystem
-     */
-    public ArrayList<Component> getComponentsInSystem() {
-        return componentsInSystem;
-    }
+	/**
+	 * @return the componentsInSystem
+	 */
+	public ArrayList<Component> getComponentsInSystem() {
+		return componentsInSystem;
+	}
 
-    public void addComponent(Component component) throws IllegalArgumentException {
-        if (component == null) {
-            throw new IllegalArgumentException("Invalid component");
-        }
+	public void addComponent(Component component) throws IllegalArgumentException {
+		if (component == null) {
+			throw new IllegalArgumentException("Invalid component");
+		}
 
-        // add to arraylist
-        componentsInSystem.add(component);
-        // update component's system
-        if (!component.getComponentSystem().equals(this)) {
-            component.setComponentSystem(this);
-        }
-    }
+		// add to arraylist
+		componentsInSystem.add(component);
+		// update component's system
+		if (!component.getComponentSystem().equals(this)) {
+			component.setComponentSystem(this);
+		}
+	}
 
-    /**
-     * This method checks if all components contained in this Artemis System are owned by a single player and returns the result as a boolean.
-     * @return - the method returns true if all components in the system are owned, otherwise it returns false.
-     */
-    public boolean checkSystemIsOwnedByOnePlayer() {
+	/**
+	 * This method checks if all components contained in this Artemis System are owned by a single player and returns the result as a boolean.
+	 * @return - the method returns true if all components in the system are owned, otherwise it returns false.
+	 */
+	public boolean checkSystemIsOwnedByOnePlayer() {
 
-        boolean ownedByOnePlayer = false;
-        List<Player> componentOwners = new ArrayList<Player>();
+		boolean ownedByOnePlayer = false;
+		List<Player> componentOwners = new ArrayList<Player>();
 
-        int counter = 0;
+		int counter = 0;
 
-        for (Component component : componentsInSystem) {
-            if (component.isOwned()) {
-                // add component owners to list
-                componentOwners.add(component.getComponentOwner());
-                counter++;
-            }
-        }
+		for (Component component : componentsInSystem) {
+			if (component.isOwned()) {
+				// add component owners to list
+				componentOwners.add(component.getComponentOwner());
+				counter++;
+			}
+		}
 
-        // continue if all components in system are owned
-        if (counter == componentsInSystem.size()) {
+		// continue if all components in system are owned
+		if (counter == componentsInSystem.size()) {
 
-            // convert list to HashSet since all values must be distinct in HashSet
-            HashSet<Player> componentOwnersSet = new HashSet<Player>(componentOwners);
+			// convert list to HashSet since all values must be distinct in HashSet
+			HashSet<Player> componentOwnersSet = new HashSet<Player>(componentOwners);
 
-            // if HashSet contains only one value then one player must own all components in system
-            ownedByOnePlayer = (componentOwnersSet.size() == 1);
-        }
+			// if HashSet contains only one value then one player must own all components in system
+			ownedByOnePlayer = (componentOwnersSet.size() == 1);
+		}
 
-        return ownedByOnePlayer;
-    }
+		return ownedByOnePlayer;
+	}
 
-    /**
-     * prints out the owner of the system for win game
-     */
-    public void displaySystemOwnerForEndGame() {
-        System.out.print("\n" + this.systemName + " was managed and fully develop by " + this.systemOwner + " with the help of their experts.");
-    }
+	/**
+	 * Loops through the components within the system and determines how many more are required before the player can
+	 * begin developing each component.
+	 *
+	 * @param player the player to check component ownership against
+	 */
+	public void announceComponentsRequiredBeforeDevelopment(Player player) throws IllegalArgumentException {
+		if (player == null) {
+			throw new IllegalArgumentException("Player is null");
+		}
 
-    /**
-     * Displays to screen information about this Artemis System
-     */
-    public void displayAllDetails() {
+		int totalComponents = this.getComponentsInSystem().size();
+		int totalOwnedByPlayer = 0;
 
-        System.out.println("System name:           \t" + systemName);
+		for (Component component : componentsInSystem) {
+			if (component.getComponentOwner() == player) {
+				totalOwnedByPlayer++;
+			}
+		}
 
-        if (systemOwner != null) {
-            System.out.println("System owner:     \t" + systemOwner.getPlayerName());
-        } else {
-            System.out.println("System owner:     \t This system is currently unowned.");
-        }
+		int remainingComponentsRequired = totalComponents - totalOwnedByPlayer;
 
-        if (checkFullyDeveloped()) {
-            System.out.println("Development Status:\t This system is fully developed. That's a big step towards the completion of the Artemis project.");
-        } else {
+		if (remainingComponentsRequired != 0) {
+			Game.announce(String.format("%s you need %s more components before you can begin developing each component within the %s",
+					player.getPlayerName().toUpperCase(), remainingComponentsRequired, systemName));
+		}
+	}
 
-            int totalDeveloped = 0;
+	/**
+	 * prints out the owner of the system for win game
+	 */
+	public void displaySystemOwnerForEndGame() {
+		System.out.print("\n" + this.systemName + " was managed and fully develop by " + this.systemOwner + " with the help of their experts.");
+	}
 
-            for (Component component : componentsInSystem) {
-                if (component.checkFullyDeveloped()) {
-                    System.out.println(component.getSquareName() + " is fully developed.");
-                    totalDeveloped++;
-                } else {
-                    System.out.println(component.getSquareName() + " is not yet fully developed. It is at development stage: " + component.getDevelopmentStage());
-                }
-            }
+	/**
+	 * Displays to screen information about this Artemis System
+	 */
+	public void displayAllDetails() {
 
-            System.out.println(totalDeveloped + " out of " + componentsInSystem.size() + " components in this system are fully developed.");
+		System.out.println("System name:           \t" + systemName);
 
-        }
+		if (systemOwner != null) {
+			System.out.println("System owner:     \t" + systemOwner.getPlayerName());
+		} else {
+			System.out.println("System owner:     \t This system is currently unowned.");
+		}
+
+		if (checkFullyDeveloped()) {
+			System.out.println("Development Status:\t This system is fully developed. That's a big step towards the completion of the Artemis project.");
+		} else {
+
+			int totalDeveloped = 0;
+
+			for (Component component : componentsInSystem) {
+				if (component.checkFullyDeveloped()) {
+					System.out.println(component.getSquareName() + " is fully developed.");
+					totalDeveloped++;
+				} else {
+					System.out.println(component.getSquareName() + " is not yet fully developed. It is at development stage: " + component.getDevelopmentStage());
+				}
+			}
+
+			System.out.println(totalDeveloped + " out of " + componentsInSystem.size() + " components in this system are fully developed.");
+
+		}
 
 
-    }
+	}
 }
